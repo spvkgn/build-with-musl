24801bd9adf69527ce3e9691a05767b378b66e33
spvkgn <spvkgn@users.noreply.github.com>
2024-06-04 10:51:00 +0500

    test

--- exhale/build.sh
+++ exhale/build.sh
@@ -2,7 +2,6 @@
 set -eo pipefail
 
 # export CFLAGS="-O3 -fno-strict-aliasing"
-export CCACHE_DIR="$GITHUB_WORKSPACE/.ccache"
 export DESTDIR=$GITHUB_WORKSPACE/out
 
 case $ARCH in
@@ -15,20 +14,16 @@ esac
 
 cd "$GITHUB_WORKSPACE/$PKG_NAME"
 
-git clone https://gitlab.com/ecodis/exhale.git && \
-mkdir -p "$PKG_NAME/_build" && \
+git clone https://gitlab.com/ecodis/exhale.git "$PKG_NAME" && \
+git -C "$PKG_NAME" tag | sort -V | tail -1 | xargs git -C "$PKG_NAME" checkout && \
 
+mkdir -p "$PKG_NAME/_build" && \
 cmake -S "$PKG_NAME" -B "$PKG_NAME/_build" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=/usr \
-  -DCMAKE_CXX_COMPILER=clang \
-  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-  -DCMAKE_LINKER_TYPE=LLD \
   -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++ -static" && \
 cmake --build "$PKG_NAME/_build" --target install/strip -- -j$(nproc) || exit 1
 
-( cd $GITHUB_WORKSPACE/out/usr/bin && tar -cJvf $GITHUB_WORKSPACE/$PKG_NAME-$PLATFORM.tar.xz exhale )
+PKG_VERSION=$(grep -Poi 'exhale version \K(\d+\.)+\d+' "$PKG_NAME/CMakeLists.txt")
 
-ccache --show-stats
-
-  # -DCMAKE_CXX_COMPILER=clang \
+( cd $GITHUB_WORKSPACE/out/usr/bin && tar -cJvf "$GITHUB_WORKSPACE/$PKG_NAME-$PKG_VERSION-$PLATFORM.tar.xz" exhale )
